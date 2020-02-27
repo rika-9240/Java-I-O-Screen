ed26fee01ddc9013b8ca5ff7e94215e836c07a7d
R.9240 <9240rika.akir0429@gmail.com>
2020-02-27 05:21:17 +0000

    javafx add

--- Screen_main.java
+++ Screen_main.java
@@ -1,5 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 public class Screen_main{
+
+    public class ScreenApplication extends Application{
+        public void start(Stage stage){
+            stage.setTitle("Java I/O Screen");
+            stage.setWidth(400);
+            stage.setHeight(500);
+            stage.show();
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        launch();
     }
 }
